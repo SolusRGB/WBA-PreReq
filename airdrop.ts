c2712a5e4c0d0a3a9c88edfb71e581c6e03c9be7
SolusRGB <brandoneddycode@gmail.com>
2024-02-15 02:11:32 +0000

    Enrolled

--- airdrop.ts
+++ airdrop.ts
@@ -1,5 +1,5 @@
 import { Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js'
-import wallet from './wallets/dev-wallet.json'
+import wallet from './wallets/wba-wallet.json'
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet))
